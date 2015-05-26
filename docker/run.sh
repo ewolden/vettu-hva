622c6ed3c859355a11dc0acd245962677d7153a0
Eivind H Wolden <ewolden@kjedelig.net>
2015-05-26 15:29:38 +0200

    testing

--- docker/run.sh
+++ docker/run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-VOLUME_HOME="/var/lib/mysql"
+# VOLUME_HOME="/var/lib/mysql"
 
-sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
-    -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php5/apache2/php.ini
-if [[ ! -d $VOLUME_HOME/mysql ]]; then
-    echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
-    echo "=> Installing MySQL ..."
-    mysql_install_db > /dev/null 2>&1
-    echo "=> Done!"  
-    /create_mysql_admin_user.sh
-else
-    echo "=> Using an existing volume of MySQL"
-fi
+# sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
+#     -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php5/apache2/php.ini
+# if [[ ! -d $VOLUME_HOME/mysql ]]; then
+#     echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
+#     echo "=> Installing MySQL ..."
+#     mysql_install_db > /dev/null 2>&1
+#     echo "=> Done!"  
+#     /create_mysql_admin_user.sh
+# else
+#     echo "=> Using an existing volume of MySQL"
+# fi
 
-#set java path for maven
-export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64
-cron -f &
-echo "=> Cron is running, harvesting performed at 03:05"
+# #set java path
+# export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64
+# cron -f &
+# echo "=> Cron is running, harvesting performed at 03:05"
 exec supervisord -n 
