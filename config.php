9bc91e19651bd93a9df757fdfd999f5d23e9cc44
Eivind <ewolden@kjedelig.net>
2015-05-27 13:43:27 +0200

    Sample config file included

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_HOST','');
+define('DB_NAME','storytelling');
+define('API_URL', 'http://api.digitaltmuseum.no/solr/');
+define('API_KEY', '');
+define('APP_MAIL', '');
+define('APP_MAIL_PASS', '');
+?>
